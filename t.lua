3cc5004fa22c51eea16944c0798ac3045b919608
Supalax <kwnn13@icloud.com>
2025-04-16 18:47:28 -0700

    Update t.lua via Python script

--- t.lua
+++ t.lua
@@ -1,1 +1,1 @@
-getgenv().myGlobalVariable = {}
+getgenv().myGlobalVariable = {"Magma": "e996a8d4-213e-484b-818c-34d1baf31f26", "Void": "62b0a356-9bfd-4e63-be82-a2886886d9ec", "Nightmare": "e996a8d4-213e-484b-818c-34d1baf31f26", "Spikey": "802d3cdc-1ee2-4877-ab3c-c3a3495d0bec", "Hell": "b5301f7c-504f-4b34-ba58-b47438317e73", "Crystal": "4fe8ec4a-3188-4a82-a007-9198c45f1368", "Rainbow": "85469010-e099-4971-bc36-415cd0687b1c", "Lunar": "563d2ebf-4bbb-4d10-9215-573c58cfb151"}
