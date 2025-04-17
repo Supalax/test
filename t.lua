b3eae2aa8a06e8676df7a924065633b509fb2887
Supalax <kwnn13@icloud.com>
2025-04-16 17:48:13 -0700

    Update t.lua with new egg data

--- t.lua
+++ t.lua
@@ -1,4 +1,12 @@
 _G.luckEggData = {
-    ["Unidentified"] = "32d52f02-50de-4058-bf6d-7fd3383ffa6a",
-    ["Rainbow"] = "95bc8033-6cf7-431d-a689-e9b4ace6bc7c",
+    ["✨ ROYAL CHEST SPAWNED ✨"] = "8f9ded93-4d3e-4b7c-b124-97d0bca47cbd",
+    ["🍀 x25 Lunar Egg Spawned! 🍀"] = "92da8fea-36a2-43b5-8026-4d425a4a94ed",
+    ["🍀 x25 Spikey Egg Spawned! 🍀"] = "4e24b398-a681-490c-953d-fe1c515d2319",
+    ["🍀 x25 Magma Egg Spawned! 🍀"] = "507171db-deea-44f3-a798-49b7c23a973e",
+    ["🍀 x25 Rainbow Egg Spawned! 🍀"] = "267eaf04-e166-4a30-ac1d-ae27d5ed1ca9",
+    ["🍀 x25 Void Egg Spawned! 🍀"] = "3f11b204-cf32-4b58-9873-11d513c11785",
+    ["🍀 x25 EGG SPAWNED 🍀"] = "219a19e3-81db-4483-a8e6-0d57b0a7bbc5",
+    ["🍀 x25 Hell Egg Spawned! 🍀"] = "cf479d61-8359-49bb-aec1-1a09dc72206b",
+    ["🍀 x25 Crystal Egg Spawned! 🍀"] = "3b597e72-088d-4e18-9d4f-956ada239e36",
+    ["🍀 x25 Nightmare Egg Spawned! 🍀"] = "d5c8c7a5-b4f7-4024-89ae-15f09eb41319",
 }
