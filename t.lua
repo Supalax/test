31008e2356e808c97490e53f52c1a40ac2ed21d0
Supalax <kwnn13@icloud.com>
2025-04-16 17:38:15 -0700

    Update t.lua with new egg data

--- t.lua
+++ t.lua
@@ -1,1 +1,12 @@
-This is the updated content of t.lua.
+_G.luckEggData = {
+    ["🍀 x25 Lunar Egg Spawned! 🍀"] = "e7ed390f-c49f-45f4-b960-245666e2fbd3",
+    ["🍀 x25 Magma Egg Spawned! 🍀"] = "770b2f29-aaf1-418d-9e7a-2730d9de248f",
+    ["🍀 x25 Crystal Egg Spawned! 🍀"] = "e90b585f-3f55-4714-aa12-7f9d438c56cc",
+    ["🍀 x25 Nightmare Egg Spawned! 🍀"] = "49172c9e-9576-41d8-8685-78078fc9af70",
+    ["🍀 x25 EGG SPAWNED 🍀"] = "3172c7b5-4316-47b7-ae3c-be802c08682b",
+    ["🍀 x25 Rainbow Egg Spawned! 🍀"] = "d99a7315-6538-42d1-b187-2a16c3950e91",
+    ["🍀 x25 Hell Egg Spawned! 🍀"] = "dece6e2c-e4eb-40fd-bf53-7607b695c77a",
+    ["🍀 x25 Spikey Egg Spawned! 🍀"] = "aa2eca82-6a32-4e71-b52f-2ab6b80e1302",
+    ["🍀 x25 Void Egg Spawned! 🍀"] = "ae294ee9-2edc-423c-9d07-c6c1c017fab2",
+    ["✨ ROYAL CHEST SPAWNED ✨"] = "289cd6c1-ebfd-44d5-b9e8-7942f544faf7",
+}
