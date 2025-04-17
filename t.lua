75197641721d256c900493d70077557c32485270
Supalax <kwnn13@icloud.com>
2025-04-16 18:40:33 -0700

    Update t.lua via Python script

--- t.lua
+++ t.lua
@@ -1,1 +1,1 @@
-getgenv().myGlobalVariable = {"\ud83c\udf40 x25 EGG SPAWNED \ud83c\udf40": "6a7414b0-f324-4cec-acfd-844431ba9e95", "\ud83c\udf40 x25 Hell Egg Spawned! \ud83c\udf40": "415c1d9f-bed5-430b-baa7-b586cd6b866a"}
+getgenv().myGlobalVariable = {}
