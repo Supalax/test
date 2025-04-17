c49a3922976fa735c569f09eb5e22eb9053338e8
Supalax <kwnn13@icloud.com>
2025-04-16 18:05:06 -0700

    Update t.lua via Python script

--- t.lua
+++ t.lua
@@ -1,1 +1,1 @@
-_G.luckEggData = {"\ud83c\udf40 x25 Nightmare Egg Spawned! \ud83c\udf40": "8b2a5167-793d-4563-bc1f-7654ca5671cb"}
+_G.luckEggData = {"\ud83c\udf40 x25 EGG SPAWNED \ud83c\udf40": "51bed07e-3cb8-4015-95d1-b5f38d706960", "\ud83c\udf40 x25 Spikey Egg Spawned! \ud83c\udf40": "4dbdcbea-7d40-477b-8d4d-ad9d35016649", "\ud83c\udf40 x25 Void Egg Spawned! \ud83c\udf40": "dd976e6e-0578-4bd8-b0e3-aa176626acbd"}
